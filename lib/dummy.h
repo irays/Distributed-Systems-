e5490aaa29973eafdfd21a6ef8d14bf2ab4219ee
Mifta <irays.teknology.ltd@gmail.com>
2013-05-25 07:25:10 +0600

    Step 5 finished

--- lib/dummy.h
+++ lib/dummy.h
@@ -1,6 +1,6 @@
 
-#ifndef__Distributed-Systems-_DUMMY_H__
-#define__Distributed-Systems-_DUMMY_H__
+#ifndef __Distributed-Systems-_DUMMY_H__ 
+#define __Distributed-Systems-_DUMMY_H__ 
 
 void dummy_hello(void);
 #endif
