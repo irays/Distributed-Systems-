ed3890d2c4977a3ddb02ec89e4cd95848be617bc
Mifta <irays.teknology.ltd@gmail.com>
2013-05-25 07:27:00 +0600

    added folders

---
+++ server/server.c
@@ -0,0 +1,16 @@
+
+//server.c contents
+
+#include <stdio.h>
+#include "../lib/dummy.h"
+
+int
+main(int argc, char **argv)
+{
+    printf("server main: HELLO\n");
+    dummy_hello();
+    printf("server main: GOODBYE\n");
+    return 0;
+}
+
+
